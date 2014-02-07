fbdc7e9909bda9f6b83831e3169b525f37151be8
Ted Angel <design@tedangel.com>
2014-02-06 23:57:46 -0800

    Commits 020714

--- sites/all/themes/wingman/user-register.tpl.php
+++ sites/all/themes/wingman/user-register.tpl.php
@@ -8,9 +8,13 @@
 
 
 <?php 
+	dpm($form);
+
   print render($form['form_id']);
   print render($form['form_build_id']);
   print render($form['account']['mail']);
+  print render($form['account']['conf_mail']);
+  print render($form['account']['pass']);
   print render ($form['field_first_name']);
   print render ($form['field_last_name']);
   print render ($form['field_user_location']);
